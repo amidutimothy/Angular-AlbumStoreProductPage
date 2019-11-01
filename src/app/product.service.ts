ee5baf26c2b02db7255d0b166a17d2919a5e0894
timothy-martin-amidu <amidu.timothy@gmail.com>
2019-11-01 14:35:00 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,9 @@ import { Http,Response } from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { }
+  // let _albumUrl:string = "../assets/album.json";
+  constructor(private _http: Http) { 
+    let _albumUrl:string = "../assets/album.json"
+  }
 
 }
