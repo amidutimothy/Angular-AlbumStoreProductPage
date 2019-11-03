bdac093eb90310a174693e33a9729ae86c14b512
timothy-martin-amidu <amidu.timothy@gmail.com>
2019-11-03 12:50:43 +0300

    atm

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,9 +3,9 @@ import { Http,Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
-
-  constructor(private _http: Http,private _albumUrl:any) { 
-    this. _albumUrl = "../assets/album.json"
+  private _albumUrl:any ="../assets/album.json"
+  constructor(private _http: Http) { 
+   
   }
 getAlbum(id:number){
   this._http.get(this._albumUrl)
