688f712c8c406a3ba9012504dcd18d740b031281
timothy-martin-amidu <amidu.timothy@gmail.com>
2019-11-03 13:30:03 +0300

    tim

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
+import { rxjs } from 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -8,6 +9,8 @@ export class ProductService {
    
   }
 getAlbum(id:number){
-  this._http.get(this._albumUrl)
+  this._http.get(this._albumUrl).subscribe(response=>{
+    return response.json();
+  })
 }
 }
