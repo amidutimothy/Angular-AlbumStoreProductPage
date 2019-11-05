8185368a32bf622a4d1ef0aa904b72e16c9cd901
timothy-martin-amidu <amidu.timothy@gmail.com>
2019-11-05 12:56:07 +0300

    p6

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../product.service';
+import { Product } from '../product';
 import { ProductService } from '../product.service'
 
 @Component({
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service'
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products:Products[];
+  products:Product[];
 
   constructor(private _productService:ProductService) { }
 
