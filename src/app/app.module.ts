9377e8a6b1c0d8e5e4e1911288ca205d57e20da6
timothy-martin-amidu <amidu.timothy@gmail.com>
2019-11-05 17:39:44 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 const appRoutes:Routes =[
   {
     path:'products',component:ProductListComponent
+  },{
+    path:'product/:id',component:ProductPageComponent
   }
 ];
 @NgModule({
